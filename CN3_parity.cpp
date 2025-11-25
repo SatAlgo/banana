5524ca276da00589bf41ec1e54859a095b486650
SatAlgo <satyamgaikwad2787@gmail.com>
2025-11-25 05:40:48 +0530

    Add files via upload

---
+++ CN3_parity.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string data;
+    cout << "Enter the binary data: ";
+    cin >> data;
+
+    int cnt1 = 0;
+    for(char i : data) if(i == '1') cnt1++;
+
+    
+    char parity_bit;
+    
+    if (cnt1 % 2 == 0) {
+        parity_bit = '0'; 
+    } else {
+        parity_bit = '1';
+    }
+
+    
+    string trans_data = data + parity_bit;
+
+    cout << "Data to send: " << trans_data << endl;
+
+    string received_data;
+    cout << "\nEnter received data: ";
+    cin >> received_data;
+
+    cnt1 = 0;
+
+    for(char i: received_data) if(i == '1') cnt1++;
+
+    if(cnt1 % 2 == 0)
+        cout << "\nNo error detected" << endl;
+    else 
+        cout << "\nError detected" << endl;
+
+    return 0;
+}
---
+++ CN7_TCP_Client.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    int client_socket;
+    sockaddr_in server_address;
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(5555);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address))
+
+    const char* message = "Hello, TCP Server!";
+    send(client_socket, message, strlen(message), 0);
+
+    char buffer[1024] = {0};
+    read(client_socket, buffer, sizeof(buffer));
+
+    cout << "Server replied: " << buffer << endl;
+
+    close(client_socket);
+    return 0;
+}
+
+// wsl --install
+// sudo apt install build-essential
+// g++ tcp_server.cpp -o server
+// g++ tcp_client.cpp -o client
